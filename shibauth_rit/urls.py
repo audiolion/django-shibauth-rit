82a1b56cfb58c3ec9bfeaf1f1610df9746608389
Ryan Castner <castner.rr@gmail.com>
2017-05-26 11:59:55 -0400

    Namespace urls

--- shibauth_rit/urls.py
+++ shibauth_rit/urls.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 # Third Party Library Imports
 # Third Party Library Imports
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 # Local Imports
 from .views import ShibLoginView, ShibLogoutView, ShibView
 
-urlpatterns = [
+urls = [
     url(r'^$', ShibView.as_view(), name='info'),
     url(r'^login/$', ShibLoginView.as_view(), name='login'),
     url(r'^logout/$', ShibLogoutView.as_view(), name='logout'),
+]
+
+urlpatterns = [
+    include(r'^$', urls, namespace='shib')
 ]
