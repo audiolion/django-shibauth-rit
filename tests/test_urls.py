5d93e7249c071a4573e7fa203ee5c9dabab03f5b
Ryan Castner <castner.rr@gmail.com>
2017-05-30 14:19:37 -0400

    Fix test

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -8,6 +8,14 @@ from django.test.utils import override_settings
 # First Party Library Imports
 from shibauth_rit.compat import reverse
 
+try:
+    from importlib import reload  # python 3.4+
+except ImportError:
+    try:
+        from imp import reload  # for python 3.2/3.3
+    except ImportError:
+        pass  # this means we're on python 2, where reload is a builtin function
+
 
 class TestDebugUrls(SimpleTestCase):
 
@@ -17,5 +25,6 @@ class TestDebugUrls(SimpleTestCase):
 
     @override_settings(DEBUG=False, SHIBAUTH_TESTING=False)
     def test_shibauth_info_url_doesnt_exist_debug_false(self):
-        res = self.client.get(reverse('shibauth_rit:shibauth_info'))
+        reload(settings)
+        res = self.client.get('/shib/')
         self.assertEqual(res.status_code, 404)
