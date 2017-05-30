61ddfe9b27f24b59de93e7b3e9662b48e8478f30
Ryan Castner <castner.rr@gmail.com>
2017-05-30 09:18:58 -0400

    Namespace urls fix

--- shibauth_rit/urls.py
+++ shibauth_rit/urls.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # Third Party Library Imports
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 # Local Imports
 from .views import ShibLoginView, ShibLogoutView, ShibView
 
-urlpatterns = [
-    url(r'^$', ShibView.as_view(), name='shibauth_info', app_name='shibauth_rit'),
-    url(r'^login/$', ShibLoginView.as_view(), name='shibauth_login', app_name='shibauth_rit'),
-    url(r'^logout/$', ShibLogoutView.as_view(), name='shibauth_logout', app_name='shibauth_rit'),
+shibauth_urlpatterns = [
+    url(r'^$', ShibView.as_view(), name='shibauth_info'),
+    url(r'^login/$', ShibLoginView.as_view(), name='shibauth_login'),
+    url(r'^logout/$', ShibLogoutView.as_view(), name='shibauth_logout'),
 ]
+
+url(r'^$', include(shibauth_urlpatterns, app_name='shibauth_rit'))
