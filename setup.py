fad027fb2421b5ebd65868af5eb00241062751f3
Ryan Castner <castner.rr@gmail.com>
2017-05-30 16:41:02 -0400

    Revert setup.py version, remove from bumpversion

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = 1.0.0
+version = get_version("shibauth_rit", "__init__.py")
 
 
 if sys.argv[-1] == 'publish':
